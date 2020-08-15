92d7eaea08b571a774ca6845ecd2fee2063d1a11
mark-soro <napoleon_soro@rocketmail.com>
2020-08-15 16:41:42 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var express = require('express')
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('search')
+});
+
+app.get('/results', function(req, res){
+  var query = req.query.search;
+  var url = `http://www.omdbapi.com/?s=${query}&apikey=57483d04`
+  console.log(url);
+  request(url, function(error, response, body) {
+    if(!error && response.statusCode == 200){
+      var parsedData = JSON.parse(body);
+      res.render('results',{results: parsedData});
+        }
+    });
+});
+
+app.listen(3000, function(){
+  console.log('Server has started');
+});
